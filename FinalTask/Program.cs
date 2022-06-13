87b45a934d5947b652d72fccba95045c7a91dd53
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-06-13 20:13:06 +0300

    добавлена функция считывания массива через пробел

---
+++ FinalTask/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите слова или сочетания букв через пробел: ");
+String[] size = Console.ReadLine().Split().ToArray();
+
+Console.Clear();
+Console.WriteLine("Введенные Вами слова: ");
+for (int i = 0; i < size.Length; i++) {
+    Console.WriteLine(size[i]);
+}
