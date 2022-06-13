ef49baaa87d0c5c70ab5a4afe11db386113cf1e5
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-06-13 20:19:50 +0300

    добавлена функция фильтрации строк с длинной >= 3

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -5,6 +5,13 @@ String[] size = Console.ReadLine().Split().ToArray();
 Console.Clear();
 Console.WriteLine("Введенные Вами слова: ");
 for (int i = 0; i < size.Length; i++) {
-    Console.WriteLine(size[i]);
+    Console.Write(size[i] + " ");
 }
+Console.WriteLine();
 
+Console.WriteLine("Введенные Вами слова с длинною больше или равной 3: ");
+for (int i = 0; i < size.Length; i++) {
+    if (size[i].Length <= 3) {
+        Console.Write(size[i] + " ");
+    }
+}
